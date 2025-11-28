e6605d240c6f822222e5175035d8251050c5c38a
Tai D. Nguyen <duytai.cse@gmail.com>
2025-11-28 15:47:15 +0800

    parse violation

--- main.py
+++ main.py
@@ -2,23 +2,19 @@ from tqdm import tqdm
 from rich import print
 from functools import partial
 from utils import ParseOptions
+from typing import Set
 from datasets import Dataset, concatenate_datasets as concat
 import dataset, llm, utils
 
-def parse_violation(name: str, option: ParseOptions) -> Dataset:
-    articles = dataset.load_articles(name)
+def parse_violation(articles: Set[str], option: ParseOptions) -> Dataset:
     examples = dataset.load_examples('violation')
     to_violation = partial(utils.to_example, 'Article', 'Violation')
 
     examples = examples.map(to_violation)
     result = Dataset.from_list([])
-    visited = set()
 
     for _ in tqdm(range(option.rounds)):
-        for article in articles['content']:
-            if article in visited:
-                continue
-
+        for article in sorted(articles):
             merged = concat([examples, result])
             choice = merged.shuffle(42).select(range(option.shots))
             few_shot = '\n\n'.join([x['example'] for x in choice])
@@ -29,19 +25,25 @@ def parse_violation(name: str, option: ParseOptions) -> Dataset:
                 _item = to_violation(dict(input=article, output=violation))
                 result = result.add_item(_item)
                 continue
-
-            visited.add(article)
+            articles.remove(article)
 
     return result.remove_columns('example')
 
-def parse_scenario(name: str, option: ParseOptions):
+def parse_scenario(violations: Set[str], option: ParseOptions):
     examples = dataset.load_examples('scenario')
-    to_violation = partial(utils.to_example, 'Violation', 'Scenario')
-    for example in examples:
-        print(example)
+    to_scenario = partial(utils.to_example, 'Violation', 'Scenario')
+
+    examples = examples.map(to_scenario)
+    result = Dataset.from_list([])
+
+    for violation in sorted(violations):
+        print(f'[bold green]Violation: [/bold green] {violation}')
 
 if __name__ == '__main__':
     parse_option = ParseOptions()
-    for item in parse_violation('audiovisual_media', parse_option):
-        print(f'[bold green]Violation:[/bold green] {item["output"]}')
-    parse_scenario('audiovisual_media', parse_option)
+
+    data = set(dataset.load_articles('audiovisual_media')['content'])
+    violations = parse_violation(data, parse_option)
+
+    data = set(violations['output'])
+    parse_scenario(data, parse_option)
